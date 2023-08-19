59a2326accddb5e8714aac9dbfb8bce5c95060f0
chaitanya6416 <katakam6416@gmail.com>
2023-08-20 01:09:51 +0530

    spawn a thread on /start_delivery

--- routes.py
+++ routes.py
@@ -30,3 +30,16 @@ def setup_routes(app: FastAPI):
         # Store serialized payload in Redis for delivery
         redis_client.lpush("delivery_requests", payload_json)
         return {"message": "Payload collected and stored"}
+
+    @app.post("/start_delivery")
+    async def start_delivery(port: int):
+        thread = DeliveryThread(port=port)
+
+        # Initialize the delivered count for this port in Redis
+        # we should not send older requests already in redis
+        # so, setting delivered_key value to number of requests already in redis cache
+        redis_client.set(thread.delivered_key, int(redis_client.get("sequence_number") or 0))
+
+        thread.start()
+        delivery_threads.append(thread)
+        return {"message": f"Delivery thread for port {port} started"}
