6f9088d4b3d40b1326128b66e09e74f3fdc121ff
chaitanya6416 <katakam6416@gmail.com>
2023-08-20 12:03:45 +0530

    fix test_routes

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -2,11 +2,11 @@ import sys
 import os
 import pytest
 from fastapi.testclient import TestClient
-from unittest.mock import MagicMock
 from fastapi import FastAPI
-from routes import setup_routes
-from redis_client import redis_client  # Import the actual redis_client here
-
+from src.routes import setup_routes
+from src.redis_client import redis_client
+import mock
+import json
 
 # Mocked Redis client
 class MockRedisClient:
@@ -19,30 +19,32 @@ class MockRedisClient:
 
     def lpush(self, key, value):
         self.data[key] = self.data.get(key, [])
-        self.data[key].insert(0, value)
-
-
-# Mock FastAPI app
-class MockApp(FastAPI):
-    def __init__(self):
-        super().__init__()
-        setup_routes(self)
-
+        self.data[key].append(value)
 
 @pytest.fixture
 def client():
+    # Create a custom mock Redis client behavior
     mock_redis = MockRedisClient()
-    app = MockApp()
-    app.dependency_overrides[redis_client] = mock_redis
-    return TestClient(app)
 
+    # Create a mock FastAPI app for testing
+    app = FastAPI()
+    setup_routes(app)
 
-def test_collect_api(client):
-    payload = {"user_id": "user123", "payload": "data"}
+    # Patch the redis_client to return the mock instance
+    with mock.patch("src.routes.redis_client", new=mock_redis):
+        # Override the redis_client dependency in the app with the mock instance
+        app.dependency_overrides[redis_client] = mock_redis
+        yield TestClient(app)
+
+def test_collect_api_positive(client):
+    # Send a valid request with the `user_id` and `payload` fields using the testing client.
+    payload = {"user_id": "123456", "payload": "This is a test payload"}
     response = client.post("/collect_api", json=payload)
+
+    # Verify that the mock Redis client's incr and lpush methods were called as expected.
     assert response.status_code == 200
     assert response.json() == {"message": "Payload collected and stored"}
 
-
-if __name__ == "__main__":
-    pytest.main(["-sv", __file__])
+    # Verify that the mock Redis client's incr and lpush methods were called as expected.
+    assert client.app.dependency_overrides[redis_client].incr("sequence_number") > 0
+    assert client.app.dependency_overrides[redis_client].lpush("delivery_requests", json.dumps(payload)) == None
