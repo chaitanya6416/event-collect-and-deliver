a5852fa8020f1a52fb13c1436805b828a06acb35
chaitanya6416 <katakam6416@gmail.com>
2023-08-21 21:03:14 +0530

    added test cases for delivery thread behaviour

--- tests/test_collect_api.py
+++ tests/test_collect_api.py
@@ -1,13 +1,23 @@
 from fastapi.testclient import TestClient
 
-from src.main import app 
+from src.main import app
 from src.redis_client import redis_client
 client = TestClient(app)
 
 
-def test_collect_api():
+def test_postive_collect_api():
+    redis_client.flushall()
     payload = {"user_id": "123456", "payload": "This is a test payload"}
     response = client.post("/collect_api", json=payload)
     assert response.status_code == 200
     assert response.json() == {"message": "Payload collected and stored"}
     assert redis_client.get("sequence_number") == '1'
+    redis_client.flushall()
+
+
+def test_negative_collect_api():
+    redis_client.flushall()
+    payload = {"payload": "This is a test payload"}
+    response = client.post("/collect_api", json=payload)
+    assert response.status_code == 400
+    redis_client.flushall()
---
+++ tests/test_start_delivery.py
@@ -0,0 +1,34 @@
+import requests
+import threading
+import time
+import unittest
+from unittest.mock import MagicMock, patch
+from fastapi.testclient import TestClient
+
+from src.main import app
+from src.redis_client import redis_client
+from src.delivery_thread import DeliveryThread
+client = TestClient(app)
+
+
+def test_start_delivery_and_check_threads():
+    redis_client.flushall()
+    threads_at_begin = threading.active_count()
+
+    # Make the request to start the delivery thread
+    response = client.post("/start_delivery?port=5555")
+
+    assert response.status_code == 200
+    assert response.json() == {
+        "message": "Delivery thread for port 5555 started"}
+
+    # Wait for the thread to start
+    time.sleep(1)
+
+    # Get the number of threads running
+    threads_at_end = threading.active_count()
+
+    # Assert that there is one thread running
+    assert threads_at_end - threads_at_begin == 1
+    client.post("/stop_delivery")
+    redis_client.flushall()
