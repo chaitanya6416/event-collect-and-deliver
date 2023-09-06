44a4c587db61eb7d56bbe91a5ebb85b0ba1188c4
chaitanya6416 <katakam6416@gmail.com>
2023-09-06 22:02:15 +0530

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,11 +1,16 @@
 ''' some dynamic or whole module wide used variables are here '''
+import os
 
-REDIS_HOST = 'localhost'
-REDIS_PORT = 6379
-REDIS_DB = 0
 
-RETRY_ATTEMPTS = 3
-WAIT_BETWEEN_REQUESTS = 1
+REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
+REDIS_PORT = os.getenv('REDIS_PORT', 6379)
+REDIS_DB = os.getenv('REDIS_DB', 0)
+
+RETRY_ATTEMPTS = os.getenv('RETRY_ATTEMPTS', 5)
+WAIT_BETWEEN_REQUESTS = os.getenv('WAIT_BETWEEN_REQUESTS', 2)
+
+RDB_BACKUPS_INTERVAL = os.getenv('RDB_BACKUPS_INTERVAL', 20)
+AOF_BACKUPS_INTERVAL = os.getenv('AOF_BACKUPS_INTERVAL', 1)
 
 delivery_threads = []
 
