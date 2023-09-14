bc79d3c281af623f0dd6f009151c3c2b0ae6ef58
chaitanya6416 <katakam6416@gmail.com>
2023-09-14 13:34:55 +0530

    update tests for backup_tasks

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,3 +4,11 @@ import sys
 # Adjust this path to point to your project's src directory
 src_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../src"))
 sys.path.insert(0, src_path)
+
+
+TEMP_ENV_VARS = {
+        'RDB_BACKUPS_INTERVAL': '1',
+        'AOF_BACKUPS_INTERVAL': '1'
+}
+
+os.environ.update(TEMP_ENV_VARS)
--- tests/test_backup_tasks.py
+++ tests/test_backup_tasks.py
@@ -10,21 +10,16 @@ from redis_client import RedisClient
 
 def test_rdb_aof_backup_tasks():
 
-    TEMP_ENV_VARS = {
-        'RDB_BACKUPS_INTERVAL': '1',
-        'AOF_BACKUPS_INTERVAL': '1'
-    }
-
-    os.environ.update(TEMP_ENV_VARS)
-    assert os.getenv('RDB_BACKUPS_INTERVAL') == '1'
-    assert os.getenv('AOF_BACKUPS_INTERVAL') == '1'
-
     redis_client = RedisClient().get_client_instance()
     redis_client.flushall()
 
     with TestClient(app) as client:
+        payload = {"user_id": "123456", "payload": "This is a test payload"}
+        client.post("/collect_api", json=payload)
+        client.post("/start_delivery?port=5555")
 
-        time.sleep(120)
-        # assert len(os.listdir(os.path.join(os.getcwd(), "rdb_backups"))) >= 1
-        # assert len(os.listdir(os.path.join(os.getcwd(), "aof_backups"))) >= 1
-        assert True
+        time.sleep(20)
+        assert len(os.listdir(os.path.join(os.getcwd(), "rdb_backups"))) >= 1
+        assert len(os.listdir(os.path.join(os.getcwd(), "aof_backups"))) >= 1
+
+    redis_client.flushall()
